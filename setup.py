9b68841e173ac65be7ee465c9b03d1959845d581
Daniel Kotschate <daniel@d3v0.de>
2019-01-21 16:49:11 +0100

    Initial structure and code layout.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyustools',
+      version='0.1',
+      description='A collection of essential helper tools.',
+      url='https://github.com/dkotscha/pyustools.git',
+      author='Daniel Kotschate',
+      author_email='daniel@d3v0.de',
+      license='MIT',
+      packages=['pyustools'],
+      zip_safe=False)
